76c75e0459c2d9ec618dc9c207c87886d51d7154
Xuffasch <tristan.lucas@gmail.com>
2019-08-05 12:51:18 +0200

    Setup Author link with corresponding layout

--- code/4+/express-locallibrary-tutorial/models/author.js
+++ code/4+/express-locallibrary-tutorial/models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -27,6 +28,18 @@ AuthorSchema
 .virtual('url')
 .get(function () {
     return '/catalog/author' + this._id;
+});
+
+AuthorSchema
+.virtual('date_of_birth_yyyy_mm_dd')
+.get(function() {
+    return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+});
+
+AuthorSchema
+.virtual('date_of_death_yyyy_mm_dd')
+.get(function() {
+    return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
 });
 
 // Export model
